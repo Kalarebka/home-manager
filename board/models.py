76bb574e3c153a9f423ffc07d5be9d5f14e79131
Ewelina Sitarczyk <ewelina.sitarczyk@gmail.com>
2022-02-14 11:06:23 +0100

    Add Models and authentication templates

--- board/models.py
+++ board/models.py
@@ -1,18 +1,52 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Board(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    max_wip = models.IntegerField(default=5)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class UserProfile(models.Model):
-    picture = models.FileField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
 
 
 class Task(models.Model):
-    board = models.ForeignKey(Board)
-    created_by = models.ForeignKey(User)
-    assigned_to = models.ForeignKey(User)
-    priority = models.IntegerChoices()
+    HIGH = 'a'
+    MEDIUM = 'b'
+    LOW = 'c'
+    priority_choices = [
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low')
+    ]
+
+    status_choices = [
+        ('todo', 'To Do'),
+        ('wip', 'In Progress'),
+        ('done', 'Done')
+    ]
+
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    priority = models.CharField(choices=priority_choices, default=MEDIUM, blank=True, max_length=1)
+    title = models.CharField(max_length=256)
+    description = models.TextField(max_length=1024, default='', blank=True)
+    status = models.CharField(choices=status_choices, default='todo', max_length=4)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
 
--- board/views.py
+++ board/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from .models import Board, Task, UserProfile
+from django.contrib.auth import authenticate, login, logout
 
 
 class IndexView(View):
+    def get(self, request):
+        context_dict = {}
+        return render("index.html", context=context_dict)
+
+
+class DisplayBoardView(View):
+    def get(self, request, board_id):
+        board = Board.objects.filter(id=board_id)
+
+
+class AddTaskView(View):
     pass
+
+
+class CreateBoardView(View):
+    pass
+
+
+class UserProfileView(View):
+    pass
+
+
+class EditTaskView(View):
+    pass
+
+
